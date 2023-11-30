446083b0a0522ca53af50843f7782089a2ac0386
josha <josha@LAPTOP-PN7OE63V>
2023-11-30 20:23:11 +0000

    Add project files.

---
+++ Day 1 part 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1_part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+
+                }
+            }
+
+        }
+    }
+}
