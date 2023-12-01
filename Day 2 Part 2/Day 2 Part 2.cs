93c273bcfd8a3d267824aa77ccbe34851ce821de
josha <josha@LAPTOP-PN7OE63V>
2023-12-01 18:47:22 +0000

    Day 2 starts added

---
+++ Day 2 Part 2/Day 2 Part 2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2_Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //coppy code from part one and modify unless not applicable
+        }
+    }
+}
---
+++ Day 2/Part 1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> file = new List<string>();
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    file.Add(sr.ReadLine());
+                }
+            }
+        }
+    }
+}
