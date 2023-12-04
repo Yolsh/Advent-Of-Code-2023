bbca1f9b4fd8a3dc7dac0437f7036dec59577dc2
josha <josha@LAPTOP-PN7OE63V>
2023-12-04 21:43:00 +0000

    day 5 prep

---
+++ Day 5 Part 1/Day 5 Part 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_5_Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> file = new List<string>();
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    file.Add(sr.ReadLine());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
