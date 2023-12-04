d00aed8ee2785fe1007e7434fa72d189f6b7f273
josha <josha@LAPTOP-PN7OE63V>
2023-12-04 19:37:41 +0000

    still cant do Day 3 Part 2

--- Day 3 Part 2/Day 3 Part 2.cs
+++ Day 3 Part 2/Day 3 Part 2.cs
@@ -33,41 +33,38 @@ namespace Day_3_Part_1
             return int.Parse(Output);
         }
 
-        static int Cont(List<int> ints, int findThis)
+        static List<int> Cont(List<int> lines, List<int> numbers)
         {
-            int total = 0;
-            for (int i = 0; i < ints.Count; i++) 
+            for (int i = 0; i < lines.Count - 1; i++)
             {
-                if (ints[i] == findThis)
+                if (lines[i] == lines[i + 1] && numbers[i] == numbers[i+1])
                 {
-                    total++;
-                }
-            }
-            return total;
-        }
-
-        static int Multiplier(List<int> numbers)
-        {
-            int total = 1;
-            for (int i = 0; i <= numbers.Count - 1; i++)
-            {
-                if (numbers[i] == 43)
-                {
-
-                }
-                if (Cont(numbers, numbers[i]) > 1)
-                {
-                    if (numbers.Count == 2)
-                    {
-                        numbers.Insert(i, 0);
-                    }
-                    else
-                    {
-                        numbers.Insert(i, 1);
-                    }
+                    //if ()
+                    numbers.Insert(i, 1);
                     numbers.Remove(numbers[i + 1]);
                 }
             }
+            if (numbers.Count <= 2 && numbers.Contains(1) && (numbers[0] < 2 || numbers[1] < 2))
+            {
+                numbers.Clear(); numbers.Add(0);
+            }
+            else if (numbers.Count <= 3 && numbers.Contains(1))
+            {
+                numbers.RemoveAll(number => number == 1);
+            }
+            return numbers;
+        }
+
+        static int Multiplier(List<int> numbers, List<int> lines)
+        {
+            int total = 1;
+            Console.Write("[");
+            foreach (int line in lines)
+            {
+                Console.Write($"{line}, ");
+            }
+            Console.WriteLine("]: ");
+            numbers = Cont(lines, numbers);
             Console.Write("[");
             foreach (int number in numbers)
             {
@@ -101,6 +98,7 @@ namespace Day_3_Part_1
         static int Collector(List<string> file, int CharNum, int LineNum)
         {
             List<int> numbersFound = new List<int>();
+            List<int> FoundNumsLine = new List<int>();
             if (CharNum > 0 && CharNum < 139)
             {
                 if (LineNum > 0 && LineNum < 139)
@@ -108,39 +106,47 @@ namespace Day_3_Part_1
                     if (Char.IsNumber(file[LineNum - 1][CharNum - 1]))
                     {
                         numbersFound.Add(FindNumber(CharNum - 1, file[LineNum - 1]));
+                        FoundNumsLine.Add(LineNum - 1);
                     }
                     if (Char.IsNumber(file[LineNum - 1][CharNum]))
                     {
                         numbersFound.Add(FindNumber(CharNum, file[LineNum - 1]));
+                        FoundNumsLine.Add(LineNum - 1);
                     }
                     if (Char.IsNumber(file[LineNum - 1][CharNum + 1]))
                     {
                         numbersFound.Add(FindNumber(CharNum + 1, file[LineNum - 1]));
+                        FoundNumsLine.Add(LineNum - 1);
                     }
                     if (Char.IsNumber(file[LineNum][CharNum - 1]))
                     {
                         numbersFound.Add(FindNumber(CharNum - 1, file[LineNum]));
+                        FoundNumsLine.Add(LineNum);
                     }
                     if (Char.IsNumber(file[LineNum][CharNum + 1]))
                     {
                         numbersFound.Add(FindNumber(CharNum + 1, file[LineNum]));
+                        FoundNumsLine.Add(LineNum);
                     }
                     if (Char.IsNumber(file[LineNum + 1][CharNum - 1]))
                     {
                         numbersFound.Add(FindNumber(CharNum - 1, file[LineNum + 1]));
+                        FoundNumsLine.Add(LineNum + 1);
                     }
                     if (Char.IsNumber(file[LineNum + 1][CharNum]))
                     {
                         numbersFound.Add(FindNumber(CharNum, file[LineNum + 1]));
+                        FoundNumsLine.Add(LineNum + 1);
                     }
                     if (Char.IsNumber(file[LineNum + 1][CharNum + 1]))
                     {
                         numbersFound.Add(FindNumber(CharNum + 1, file[LineNum + 1]));
+                        FoundNumsLine.Add(LineNum + 1);
                     }
                 }
             }
             Console.Write($"{ LineNum }: ");
-            return Multiplier(numbersFound);
+            return Multiplier(numbersFound, FoundNumsLine);
         }
 
         static void Main(string[] args)
