f71e1398ff76619289623ec23a1bd8fcfd3d62af
josha <josha@LAPTOP-PN7OE63V>
2023-12-07 06:22:41 +0000

    day 7 start

--- Day 7 Part 1/Day 7 Part 1.cs
+++ Day 7 Part 1/Day 7 Part 1.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.CodeDom.Compiler;
 
 namespace Day_7_Part_1
 {
     internal class Program
     {
+        static char[] cardTypes = { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' };
+        struct card
+        {
+            public int Length;
+            public string cardSet;
+            public string fullCard;
+        }
+
         static void Main(string[] args)
         {
             List<string> file = new List<string>();
+            List<card> cards = new List<card>();
             using (StreamReader sr = new StreamReader("input.txt"))
             {
                 while(!sr.EndOfStream)
@@ -19,7 +30,41 @@ namespace Day_7_Part_1
                     file.Add(sr.ReadLine());
                 }
             }
+            foreach (string line in file)
+            {
+                cards.Add(HouseFinder(line));
+            }
             Console.ReadKey();
+        }
+
+        static card HouseFinder(string line)
+        {
+            card temp = new card(); temp.Length = 0; temp.cardSet = "I"; temp.fullCard = line;
+            foreach (char card in cardTypes)
+            {
+                Regex re = new Regex($"[{card}]+");
+                Match match = re.Match(line);
+                if (match.ToString().Length > temp.Length)
+                {
+                    temp.Length = match.ToString().Length;
+                    temp.cardSet = match.ToString();
+                }
+            }
+            return temp;
+        }
+
+        static int HouseCompare(List<card> cards)
+        {
+            for (int i = 0; i < cards.Count; i++) 
+            {
+                if (cards[i].Length < cards[i+1].Length)
+                {
+                    cards.Insert(i, cards[i+1]);
+                    cards.Remove(cards.ElementAt(i+1)); //this may need changing
+                    cards.Remove(cards.ElementAt(i + 2));
+                }
+
+            }
         }
     }
 }
